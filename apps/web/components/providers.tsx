54d0c12c1103e6de559a0dc5cf524817d7bcf142
Leonid Meleshin <hello@leon0399.ru>
2025-06-29 22:25:34 +0200

    feat(ui): add sonner toaster

--- apps/web/components/providers.tsx
+++ apps/web/components/providers.tsx
@@ -3,6 +3,7 @@
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 import { SessionProvider } from 'next-auth/react';
+import { Toaster } from "@workspace/ui/components/sonner"
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
@@ -15,6 +16,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
         enableColorScheme
       >
         {children}
+        <Toaster />
       </NextThemesProvider>
     </SessionProvider>
   )
