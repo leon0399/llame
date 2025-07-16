7eafc907f7c7714a6a99e2ef17c198f8de28d4ea
Leonid Meleshin <hello@leon0399.ru>
2025-07-16 06:39:26 +0200

    feat(chats): fetch user chats from API

--- apps/web/sentry.edge.config.ts
+++ apps/web/sentry.edge.config.ts
@@ -11,6 +11,10 @@ Sentry.init({
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
 
+  integrations: [
+    Sentry.consoleLoggingIntegration({ levels: ["log", "error", "warn"] }),
+  ],
+
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
 });
